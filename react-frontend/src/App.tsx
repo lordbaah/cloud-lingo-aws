7ff22a178cff77de0022295ab35b6726e23ef5e1
lordbaah <lordbaah2@gmail.com>
2025-07-08 17:19:19 +0000

    Fix GitHub Actions path

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -68,7 +68,7 @@ function App() {
           <p className="text-gray-600">
             Translate text between different languages using AWS Translate
           </p>
-          <p>This is capstone project</p>
+          <p>Added github actions</p>
         </div>
 
         {/* Main Translation Interface */}
